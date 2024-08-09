7b8cddbc3093589cc0614dbcf276fbb712103e09
aiq-9 <alexkaufman78@gmail.com>
2024-08-09 12:19:28 -0400

    26. Composting Items (misnumbered thing earlier)

--- src/main/java/net/aiq9/kaupencourse/KaupenCourse121.java
+++ src/main/java/net/aiq9/kaupencourse/KaupenCourse121.java
@@ -11,6 +11,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.ComposterBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,5 +35,8 @@ public class KaupenCourse121 implements ModInitializer {
 		ModDataComponentTypes.registerComponentTypes();
 
 		AttackEntityCallback.EVENT.register(new AttackEntityHandler());
+
+		ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.STRAWBERRY, 0.5f);
+		ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.STRAWBERRY, 0.25f);
 	}
 }
